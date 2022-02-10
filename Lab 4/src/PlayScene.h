ada9f157f4603013d0e887c90b5b7fc3b331a2f5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-02-10 12:25:43 -0500

    Cleaned the PlayScene and Modifed Ship Class

--- Lab 4/src/PlayScene.h
+++ Lab 4/src/PlayScene.h
@@ -6,7 +6,6 @@
 
 #include "Target.h"
 #include "SpaceShip.h"
-#include "Obstacle.h"
 
 class PlayScene : public Scene
 {
@@ -30,7 +29,6 @@ private:
 	// Game Objects
 	Target* m_pTarget;
 	SpaceShip* m_pSpaceShip;
-	Obstacle* m_pObstacle;
 
 	// Debug bool
 	bool m_bDebugView;
