1932088fec79d5d1a7c0e5653515e8658d297148
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-24 12:40:10 -0400

    Added PatrolAction class

--- Lab 7.1/src/ActionNode.h
+++ Lab 7.1/src/ActionNode.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef __ACTION_NODE__
 #define __ACTION_NODE__
+
 #include "TreeNode.h"
 #include "Agent.h"
 
@@ -10,10 +11,12 @@ class ActionNode : public TreeNode
 public:
 	ActionNode(): m_agent(nullptr) { m_isLeaf = true; }
 	virtual ~ActionNode() = default;
+	
 	virtual void Action() = 0;
 
 	Agent* getAgent() { return m_agent; }
 	void setAgent(Agent* agent) { m_agent = agent; }
+
 private:
 	Agent* m_agent;
 };
